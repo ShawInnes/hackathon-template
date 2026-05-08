e2ef73dc48b27518dcc2448a661aeeb31b41b58f
Shaw Innes <shaw@immortal.net.au>
2026-05-09 00:17:47 +1000

    feat: Add def origins by environment

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  allowedDevOrigins: process.env.ALLOWED_DEV_ORIGINS 
+    ? process.env.ALLOWED_DEV_ORIGINS.split(',') 
+    : [],
 };
 
 export default nextConfig;
